4e285b727bde4dac31dfd3bc16e1ec9169b99a16
AnnaSirina <sirina.anya@gmail.com>
2019-11-05 22:08:13 +0300

    flickity

--- src/scripts/rewiers.js
+++ src/scripts/rewiers.js
@@ -1,32 +1,47 @@
-import Vue from "vue";
-
+import Vue from 'vue';
 import Flickity from 'vue-flickity';
 
 new Vue({
+    el: '#review__slider-wrapper',
+    template: '#review__slider',
     components: {
         Flickity
     },
-
     data() {
         return {
+            reviews: [],
             flickityOptions: {
-                initialIndex: 3,
                 prevNextButtons: false,
                 pageDots: false,
-                wrapAround: true
-
-                // any options from Flickity can be used
+                freeScroll: false,
+                groupCells: true,
+                wrapAround: false
             }
         }
     },
-
     methods: {
-        next() {
-            this.$refs.flickity.next();
-        },
+        makeRequireImagesPath(data) {
+            return data.map(item => {
+                const requirePic = require(`../images/content/${item.photo}`);
 
-        previous() {
-            this.$refs.flickity.previous();
+                item.photo = requirePic;
+
+                return item;
+            });
+        },
+        slide(direction) {
+            switch (direction) {
+                case 'next':
+                    this.$refs.flickity.next();
+                    break;
+                case 'prev':
+                    this.$refs.flickity.previous();
+                    break;
+            }
         }
+    },
+    created() {
+        const data = require('../data/rewiers.json');
+        this.reviews = this.makeRequireImagesPath(data);
     }
 });
